5ad704b299aff56363eedec770bfc5c5078851f0
Van La <langocthuyvan@gmail.com>
2023-07-30 15:58:54 -0500

    Remove unneccessary files

--- bitc_nls_ep/Mg2EDTA/5.analysis/injection_heat/run_me.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SCRIPT="/home/tnguye46/opt/src/bayesian-itc/analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/scripts/run_injection_heat.py"
-
-export TRACE="/home/tnguye46/bayesian_itc/Mg2EDTA/2.bitc_mcmc_lognomP0_lognomLs/repeat_0/Mg1EDTAp1a/traces.pickle"
-
-python $SCRIPT --mcmc_trace_file $TRACE
-
--- bitc_nls_ep/Mg2EDTA/5.analysis/kde/nonlinear_fit/run_me.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SCRIPT="/home/tnguye46/opt/src/bayesian-itc/analysis_of_Mg2EDTA_ABRF-MIRG02_Thermolysin/scripts/run_kde_2d.py"
-
-export MCMC_DIR="/home/tnguye46/bayesian_itc/Mg2EDTA/4.gaussian_mcmc"
-
-export EXPER="Mg1EDTAp1a Mg1EDTAp1b Mg1EDTAp1c Mg1EDTAp1d Mg1EDTAp1e Mgp5EDTAp05a Mgp5EDTAp05b Mgp5EDTAp05c Mgp5EDTAp05e Mgp5EDTAp05f Mgp5EDTAp05g Mgp5EDTAp05h Mgp5EDTAp05i Mgp5EDTAp05j"
-
-python $SCRIPT --mcmc_dir $MCMC_DIR --ordered_experiment_names "$EXPER"
